0e51222db41f399345df306ac23e6f5fd0212825
night-glider <85761149+night-glider@users.noreply.github.com>
2023-02-28 09:11:35 +0300

    fixed minor bugs

--- line_counter.py
+++ line_counter.py
@@ -2,8 +2,8 @@ import pathlib
 import sys
 
 if len(sys.argv) < 3:
-	print("syntax: py line_counter.py \"path\" \"filter\"\nfilters:\n*    - return all files\n*.py - return all .py files")
-	quit()
+	print("syntax: line_counter \"path\" \"filter\"\nfilters:\n*    - return all files\n*.py - return all .py files")
+	sys.exit()
 
 path = sys.argv[1]
 filt = sys.argv[2]
@@ -16,9 +16,12 @@ total_lines = 0
 
 for file in files:
 	print(file)
-	line_count = len( open(file, 'rt', errors="ignore").readlines() )
-	print( "lines: ", line_count )
-	total_lines += line_count
+	try:
+		line_count = len( open(file, 'rt', errors="ignore").readlines() )
+		print( "lines: ", line_count )
+		total_lines += line_count
+	except:
+		print("Can't open file")
 
 print("        Total lines: ", total_lines)
 
